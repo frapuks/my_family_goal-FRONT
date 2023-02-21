8dd0873c9a5405928ec8da8d8d682de4cd95d160
Adrien BLANC <adrien.blanc.77@gmail.com>
2023-02-21 17:23:08 +0100

    Nettoyage de APP/index.jsx et création d'un component NavBar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar () {
+    return (
+        <div className="container-navBar">
+            <button>SETTING</button>
+            <button>FAMILLE</button>
+            <button>PROFIL</button>
+        </div>
+    )
+}
+
+export default NavBar;
