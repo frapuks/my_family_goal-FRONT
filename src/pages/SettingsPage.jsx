1f400b61a990f99ab204f38412de652a192ebd23
AdrienHaggani <adrien.haggani@gmail.com>
2023-02-23 16:15:05 +0100

    Ajout des boutons : modifier le profil , famille 1 et famille 2 pour tester. Ajout du renvoie des pages aux fichiers adéquate.

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -2,8 +2,25 @@ import React from "react";
 
 import NavBar from "../components/NavBar/NavBar";
 
+//Je veux creer un composant Option du profil qui va etre un bouton qui va rediriger vers la page de modification du profil
+
 function SettingsPage() {
-  return <NavBar />;
-}
+
+  const handleModifyProfile = () => {
+    window.location.href = '/UserSettingsPage';
+  };
+
+  const handleModifyFamily = () => {
+    window.location.href = '/FamilySettingsPage';
+  }
+  return (
+    <div>
+      <button onClick={handleModifyProfile}>Modifier mon profil</button>
+      <button onClick={handleModifyFamily}>Famille1</button>
+      <button>Famille2</button>
+    </div>
+
+  );
+};
 
 export default SettingsPage;
