49e72bfc9792c133b45b34aa41e61ad316964b31
AdrienHaggani <adrien.haggani@gmail.com>
2023-03-02 10:53:39 +0100

    test appel families

--- src/components/Carousel/CarouselMembre.jsx
+++ src/components/Carousel/CarouselMembre.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from "@mui/material";
 import Carousel from 'react-material-ui-carousel';
 import CardMembre from "../Cards/CardMembre";
+import { useState, useEffect } from "react";
 
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
@@ -9,41 +10,37 @@ import styles from "./Carousel.module.scss"
 
 import Diversity1OutlinedIcon from '@mui/icons-material/Diversity1Outlined';
 
-const membreData=
-[
-    {
-        "id": 1,
-        "name": "Moaï"
-    },
-    {
-        "id": 2,
-        "name": "Front"
-    },
-    {
-        "id": 3,
-        "name": "Back"
-    }
-]
+
+
 
 
 
 
 function CarouselMembres() {
 
+    const [data, setData] = useState([]);
+
+
+    useEffect(() => {
+        
+    const fetchFamilies = async () => {
+        const response = await fetch("/families");
+        const data = await response.json();
+        setData(data);
+    }
+      
+    }, []);
+
+
     return (
-            <>
-            <h2 className={styles.title}> <Diversity1OutlinedIcon/> MEMBRES<Button><AddCircleOutlineIcon sx={{color: "green"}}/></Button> </h2>
+            
+            <><h2 className={styles.title}> <Diversity1OutlinedIcon /> MEMBRES<Button><AddCircleOutlineIcon sx={{ color: "green" }} /></Button> </h2><Carousel autoPlnpay={false}>
+            {data.map((data) => <CardMembre key={data.id} {...data} />)}
+        </Carousel></>
 
-            <Carousel autoPlay={false}>
-                {
-                membreData.map( (data) => <CardMembre key={data.id} {...data}/> )
-                }
-            </Carousel>
-
-            </>
-    )
+        
+    ) 
     
 }
-
 
 export default CarouselMembres;
