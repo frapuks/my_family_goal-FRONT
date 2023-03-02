272ef7e79f4d04a0530d8a14cc0d237b1464ba51
Charlotte <charlotte@anros.fr>
2023-03-02 16:35:05 +0100

    mise en place du bouton delete

--- src/components/UserSettings/DeleteButton/DeleteButton.jsx
+++ src/components/UserSettings/DeleteButton/DeleteButton.jsx
@@ -1,28 +1,67 @@
 import styles from "../UserSettingsForm.module.scss";
 import { Button } from "../../Common/Button";
 import { Colors } from "../../../constants/Colors";
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ButtonMui from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useNavigate } from "react-router-dom";
 
 function DeleteButton() {
+    const navigate = useNavigate();
+
     const user = useSelector(state => state.user.user);
     const token = useSelector(state => state.user.token);
     console.log(user);
 
-    const onDeleteButton = async () => {
-        console.log("moi tout supprimer");
+    const [open, setOpen] = useState(false);
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const cancelDeleteClick = () => {
+        console.log("j'annule");
+        setOpen(false);
+    };
+
+    const confirmdDeleteClick = async () => {
         const response = await fetch(import.meta.env.VITE_API_ROOT + `/user/${user.id}`, {
             method: "DELETE",
             headers: { "Content-Type": "application/json", authorization: `Bearer ${token}` },
         });
 
         if (response.ok) {
-            console.log("vous avez été supprimer...");
+            console.log("je delete");
+            navigate("/");
         }
+
+        setOpen(false);
     };
+
     return (
         <div className={styles.formButton}>
-            <Button text="Supprimer le compte" color={Colors.Warning} onClick={onDeleteButton} />
+            <Button text="Supprimer le compte" color={Colors.Warning} onClick={handleClickOpen} />
+            <Dialog open={open} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+                <DialogTitle id="alert-dialog-title">{"ATTENTION"}</DialogTitle>
+
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Etes-vous sure de vouloir supprimer ce compte?
+                    </DialogContentText>
+                    <DialogActions>
+                        <ButtonMui onClick={cancelDeleteClick}>Annuler</ButtonMui>
+
+                        <ButtonMui onClick={confirmdDeleteClick} autoFocus>
+                            confirmer
+                        </ButtonMui>
+                    </DialogActions>
+                </DialogContent>
+            </Dialog>
         </div>
     );
 }
