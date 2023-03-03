aa7fb74fcc89433fa7c0ddd1420003c593e49129
Charlotte <charlotte@anros.fr>
2023-03-03 11:23:47 +0100

    rectification des bugs sur les actions

--- src/store/slices/familiesSlice.js
+++ src/store/slices/familiesSlice.js
@@ -1,15 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+    families: [],
+    selectFamily: null,
+};
+
 const familiesSlice = createSlice({
     name: "families",
-    initialState: {
-        families: [],
-        selectFamily: null,
-    },
+    initialState,
     reducers: {
         addFamily: (state, action) => {
             // state.families = [...state.families, payload];
             state.families.push(action.payload);
+        },
+        resetFamily: state => {
+            state = initialState;
+
+            return state;
         },
         setFamilies: (state, action) => {
             state.families = action.payload;
@@ -17,9 +24,13 @@ const familiesSlice = createSlice({
         setSelectFamily: (state, action) => {
             state.selectFamily = action.payload;
         },
+        resetSelectedFamily: state => {
+            state.selectFamily = initialState.selectFamily;
+            return state;
+        },
     },
 });
 
-export const { addFamily, setFamilies, setSelectFamily } = familiesSlice.actions;
+export const { addFamily, setFamilies, setSelectFamily, resetFamily, resetSelectedFamily } = familiesSlice.actions;
 
 export default familiesSlice.reducer;
