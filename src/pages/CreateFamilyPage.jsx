ae2cbf623d51383cfeca37e50968bbcd8fb5a2f9
Adrien BLANC <adrien.blanc.77@gmail.com>
2023-03-07 14:16:28 +0100

    Css page d'accueil nouvel isncrit

--- src/pages/CreateFamilyPage.jsx
+++ src/pages/CreateFamilyPage.jsx
@@ -4,12 +4,18 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { selectToken } from "../store/slices/userSlice";
 
+import { ValidateButton } from "../components/Common/ValidateButton";
+import { Colors } from "../constants/Colors";
+
 import styles from "./CreateFamilyPage.module.scss";
 
 import logo from "../assets/logo-fond-transparent-sans-police.svg";
 
 function CreateFamily() {
   const token = useSelector(selectToken);
+  const user = useSelector(state => state.user.user)
+  const pseudo = user.pseudo;  
+  
   const navigate = useNavigate();
 
   const checkToken = () => {
@@ -24,11 +30,11 @@ function CreateFamily() {
         <h1>Merci pour votre inscription</h1>
         <section className={styles.textWelcomeParents}>
           <p>Bonjour les parents, il est temps de créer votre Famille et de choisir le nom que vous souhaitez lui donner.</p>
-          <button onClick={checkToken}>Creer ma famille</button>
+          <ValidateButton text="Creer ma famille" onClick={checkToken} color={Colors.Primary} />
         </section>
         <section className={styles.textWelcomeChildrens}>
-          <p>Bonjour les enfants, merci de nous rejoindre! Il n&apos;y a plus qu&apos;a attendre l&apos;invitation à rejoindre votre famille.</p>
-        </section>      
+          <p>Bonjour les enfants ! Il n&apos;y a plus qu&apos;a attendre l&apos;invitation à rejoindre votre famille. Pour que vos parents puissent vous ajouter, vous aurez besoin de leur communiquer votre pseudo: {pseudo}. </p>
+        </section>
       </div>
   );
 }
