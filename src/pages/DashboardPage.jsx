331efef54d5e1ccb71770e536e3aac6bd82c787c
francois <francoisgrunert@gmail.com>
2023-03-07 14:09:20 +0100

    init state selectFamily if is null on display dashboard

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import styles from "./DashboardPage.module.scss";
 
@@ -8,9 +8,14 @@ import CarouselAll from "../components/Carousel/CarouselAll";
 
 //Import des reducers du store
 import { setActivePage } from "../store/slices/navBarSlice";
+import { setSelectFamily } from "../store/slices/familiesSlice";
 
 function DashboardPage() {
-    const dispatch = useDispatch();    
+    const dispatch = useDispatch();
+    // init selectFamily if null
+    const selectFamily = useSelector(state => state.families.selectFamily);
+    const familyIndex = useSelector(state => state.families.listFamilies[0]);
+    if (!selectFamily) setSelectFamily(familyIndex);
 
     // [dispatch] servira à modifier le state uniquement au changement de valeur, et non a chaque nouveau rendu d'une même page par exemple
 
